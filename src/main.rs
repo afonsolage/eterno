87f335245cd307702eca28bc9518fe3320940514
Afonso Lage <lage.afonso@gmail.com>
2022-06-16 06:51:29 -0300

    Spawn a fat and ugly character. closes #3

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,22 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // Spawn a fat and ugly terrain
+    // Spawn a flat and ugly terrain
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
         material: materials.add(Color::rgb(0.6, 8.0, 0.6).into()),
+        ..default()
+    });
+
+    // Spawn a fat and ugly character
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Capsule {
+            depth: 0.5,
+            radius: 0.125,
+            ..default()
+        })),
+        material: materials.add(Color::rgb(0.6, 0.6, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     });
 
