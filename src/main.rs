aff074da3715a9f37bad8911707c99df8a41f4ec
Afonso Lage <lage.afonso@gmail.com>
2022-06-16 06:45:33 -0300

    Spawn a flat and ugly terrain closes #2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Spawn a fat and ugly terrain
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+        material: materials.add(Color::rgb(0.6, 8.0, 0.6).into()),
+        ..default()
+    });
+
+    // Spawn a camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(0.0, 2.0, -5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
